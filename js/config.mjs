010df0685931ddce1b0f1408b75fc1b599368ac2
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-08-20 11:43:13 +0200

    Beheer: gebruik nieuwe organisation-config setup
    Part of Logius-standaarden/Automatisering#51

--- js/config.mjs
+++ js/config.mjs
@@ -1,4 +1,6 @@
-var respecConfig = {
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
   alternateFormats: [ { 
         "label" : "pdf",
         "uri" : "Digikoppeling-Koppelvlakstandaard-ebMS2.pdf"
@@ -22,4 +24,4 @@ var respecConfig = {
   shortName: "ebms",
   specStatus: "WV",
   specType: "ST"
-};
+});
