ff8df9dbfd7321c525343675edf0b9e059d6d93a
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-03-09 13:02:46 +0100

    Beheer: verwijder PDF formaat (#15)

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Koppelvlakstandaard-ebMS2.pdf"
-      } ],
   authors: [ { 
         "name" : "Logius",
         "url" : "https://logius.nl/standaarden"
